3484cac24b2388fa966d8c1f088d735415f03c3b
tonyward <tony.ward2603@gmail.com>
2020-11-10 19:04:28 +1100

    Switched to linking whole config folders to simplify config file

--- install.sh
+++ install.sh
@@ -10,16 +10,16 @@ else
 		if [[ ! $line =~ ^# ]]; then
 			src=`pwd`/`echo $line | cut -f1 -d' '`
 			dst=`echo $line | cut -f2 -d' '`
-			if [ ! -f $src ]; then
-				echo "[-] Error linking $src to $dst - source file not found"
+			if [ ! -d $src ]; then
+				echo "[-] Error linking $src to $dst - source directory not found"
 			else
-				if [ -f $dst ]; then
+				if [ -d $dst ]; then
 					echo "[-] $dst already exists - deleting first"
-					rm $dst
+					rm -rf $dst
 				fi
 				ln -s $src $dst
-				if [ ! -f $dst ]; then
-					echo "[-] Error linking $src to $dst - destination file does not exist after link"
+				if [ ! -d $dst ]; then
+					echo "[-] Error linking $src to $dst - destination directory does not exist after link"
 				else
 					echo "[+] Linked $src to $dst"
 				fi
