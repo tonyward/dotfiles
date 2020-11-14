c4558a6908c597734e8594b39fff8dd357447749
tonyward <tony.ward2603@gmail.com>
2020-11-14 13:06:49 +1100

    Fucked with polybar

--- polybar/my_bar/launch.sh
+++ polybar/my_bar/launch.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env sh
-
-## Add this to your wm startup file.
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -9,4 +7,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+polybar example -c ~/.config/polybar/config.ini >>/tmp/polybar1.log 2>&1 & disown
